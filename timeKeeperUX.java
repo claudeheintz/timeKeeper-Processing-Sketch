f58585d6bf8c4600cd45d883fab9382777d52ac2
Claude Heintz <claudeheintzdesign@gmail.com>
2018-03-07 21:32:50 -0600

    read display params from properties

--- timeKeeperUX.java
+++ timeKeeperUX.java
@@ -1,4 +1,6 @@
 import processing.core.*; 
+import java.util.*;
+import java.io.*;
 
 // timeKeeperUX is a static class designed to hold display location and size values
 //
@@ -51,7 +53,10 @@ static    long  warning_milliseconds = 120000;
 public static final int DEFAULT_UX_TYPE = 0;
 public static final int HDPI_UX_TYPE    = 1;
 
-	public static void initForApplet(PApplet p, int type) {
+  public static void initForApplet(PApplet p) {
+    initFromProperties(p);
+    
+    // these values are calculated
 	  progress_w = (3 * p.width) / 4;
   
 	  bottom_y = p.height - bottom_dy;
@@ -63,32 +68,120 @@ public static final int HDPI_UX_TYPE    = 1;
 	  elapsed_x = progress_x + 200;
 	  remaining_x = progress_x + progress_w - 200;
 	  elapsed_remaining_y = progress_y - progress_h - 30;
-  
-	  if ( type == HDPI_UX_TYPE ) {
-      progress_h = 150;
-      current_title_dy = 280;
-      
-			bottom_dy = 100;                             //50         --100
-  
-			timeadj_button_w = 100;                      //50;        --100
-			timeadj_button_h = 80;                       //40;        --80
-			timeadj_button_dx = 200;                     //100        --200
-			timeadj_button_dy = 75;                      //25         --75
-			timeadj_font_size = 48.0f;                    //24.0f      -- 48.0f
-			timeadj_text_dx = (timeadj_button_dx+timeadj_button_w)/2;
-  
-			ondeck_x = 100;                              // 100       --100
-			ondeck_font_size = 64.0f;                      // 32.0f     --64.0f
-  
-			now_font_size = 96.0f;                     // 48.0f     --96.0f
-			now_dy = 100;                            // 50        --100
-			et_rt_font_size = 160.0f;           // 72.0f     --150.0f
-			et_rt_label_font_size = 48.0f;       // 32.0f     --48.0f
-			et_rt_label_dy = 200;              // 100       --200
-  
-			current_title_font_size = 112.0f;                // 64.0f     --96.0f    
-	  }
 	}
+
+  public static void initFromProperties(PApplet p) {
+     Properties myprops = new Properties();
+  
+     FileInputStream in = null;
+     try {
+       in = new FileInputStream ( p.sketchPath("")+"/timeKeeper.properties" );
+       myprops.load(in);
+       in.close();
+      } catch (FileNotFoundException fex) {
+       in = null;
+       System.out.println("Load Preferences exception: " + fex );
+     } catch (IOException iox) {
+       in = null;
+       System.out.println("Load Preferences exception: " + iox );
+     }
+     
+     int type = getIntegerProperty(myprops, "display_type", DEFAULT_UX_TYPE, 0);
+     
+     // the following are calculated later and don't necessarily need to be read from
+     progress_h = getIntegerProperty(myprops, "progress_h", DEFAULT_UX_TYPE, progress_h);
+     progress_w = getIntegerProperty(myprops, "progress_w", DEFAULT_UX_TYPE, progress_w);
+     elapsed_x = getIntegerProperty(myprops, "elapsed_x", DEFAULT_UX_TYPE, elapsed_x);
+     elapsed_remaining_y = getIntegerProperty(myprops, "elapsed_remaining_y", DEFAULT_UX_TYPE, elapsed_remaining_y);
+     bottom_y = getIntegerProperty(myprops, "bottom_y", DEFAULT_UX_TYPE, bottom_y);
+     center_x = getIntegerProperty(myprops, "center_x", DEFAULT_UX_TYPE, center_x);
+     center_y = getIntegerProperty(myprops, "center_y", DEFAULT_UX_TYPE, center_y);
+     progress_y = getIntegerProperty(myprops, "progress_y", DEFAULT_UX_TYPE, progress_y);
+     progress_x = getIntegerProperty(myprops, "progress_x", DEFAULT_UX_TYPE, progress_x);
+     
+     // the following are dual default/HDPi fixed values
+     bottom_dy = getIntegerProperty(myprops, "bottom_dy", type, bottom_dy);
+     timeadj_button_w = getIntegerProperty(myprops, "timeadj_button_w", type, timeadj_button_w);
+     timeadj_button_h = getIntegerProperty(myprops, "timeadj_button_h", type, timeadj_button_h);
+     timeadj_button_dx = getIntegerProperty(myprops, "timeadj_button_dx", type, timeadj_button_dx);
+     timeadj_button_dy = getIntegerProperty(myprops, "timeadj_button_dy", type, timeadj_button_dy);
+     timeadj_font_size = getFloatProperty(myprops, "timeadj_font_size", type, timeadj_font_size);
+     ondeck_x = getIntegerProperty(myprops, "ondeck_x", type, ondeck_x);
+     ondeck_font_size = getFloatProperty(myprops, "ondeck_font_size", type, ondeck_font_size);
+     now_font_size = getFloatProperty(myprops, "now_font_size", type, now_font_size);
+     now_dy = getIntegerProperty(myprops, "now_dy", type, now_dy);
+     et_rt_font_size = getFloatProperty(myprops, "et_rt_font_size", type, et_rt_font_size);
+     et_rt_label_font_size = getFloatProperty(myprops, "et_rt_label_font_size", type, et_rt_label_font_size);
+     et_rt_label_dy = getIntegerProperty(myprops, "et_rt_label_dy", type, et_rt_label_dy);
+     current_title_font_size = getFloatProperty(myprops, "current_title_font_size", type, current_title_font_size);
+     warning_milliseconds = getLongProperty(myprops, "warning_milliseconds", type, warning_milliseconds);
+     
+     //calculated from above
+     timeadj_text_dx = (timeadj_button_dx+timeadj_button_w)/2;
+     timeadj_text_dy = timeadj_button_dy - (int)((timeadj_button_h+timeadj_font_size)/2);
+  }
+
+  public static int getIntegerProperty(Properties myprops, String kstr, int type, int def) {
+    String pstr;
+  
+    if ( type == HDPI_UX_TYPE ) {
+      pstr = (String)myprops.get(kstr+"1");
+    } else {
+      pstr = (String)myprops.get(kstr);
+    }
+    if ( pstr != null ) {
+      return safeParseInt(pstr);
+    }
+    return def;
+  }
+  
+  public static float getFloatProperty(Properties myprops, String kstr, int type, float def) {
+    String pstr;
+    if ( type == HDPI_UX_TYPE ) {
+      pstr = (String)myprops.get(kstr+"1");
+    } else {
+      pstr = (String)myprops.get(kstr);
+    }
+    if ( pstr != null ) {
+      return safeParseFloat(pstr);
+    }
+    return def;
+  }
+  
+  public static long getLongProperty(Properties myprops, String kstr, int type, long def) {
+    String pstr;
+    if ( type == HDPI_UX_TYPE ) {
+      pstr = (String)myprops.get(kstr+"1");
+    } else {
+      pstr = (String)myprops.get(kstr);
+    }
+    if ( pstr != null ) {
+      return safeParseLong(pstr);
+    }
+    return def;
+  }
+  
+  public static float safeParseFloat(String f) {
+    try {
+      return( Float.valueOf(f).floatValue() );
+    } catch ( NumberFormatException e ) {}
+      catch (NullPointerException ne ) {}
+    return (0);
+  }
+  
+  public static long safeParseLong(String f) {
+    try {
+      return( Long.valueOf(f).longValue() );
+    } catch ( NumberFormatException e ) {}
+    return (0);
+  }
+  
+  public static int safeParseInt(String s) {
+    try {
+      return( Integer.parseInt(s) );
+    } catch ( NumberFormatException e ) {}
+    return (0);
+  }
 
   //  button location and size =========================================
 
